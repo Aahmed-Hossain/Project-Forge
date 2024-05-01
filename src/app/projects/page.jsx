4787ef648c0ca231ac214d820b5aaceabe8840af
Aahmed-Hossain <aahmdctec@gmail.com>
2024-05-01 11:14:49 +0600

    card ready to show details of a project

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import { Card } from 'antd';
-import { EditOutlined, DeleteOutlined ,BorderTopOutlined } from '@ant-design/icons';
+import React from "react";
+import { Card } from "antd";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  ArrowsAltOutlined ,
+} from "@ant-design/icons";
 
 const ProjectPage = () => {
   return (
-    <div className='m-4'>
-    <Card bordered={true} hoverable={true} title="Project Forge" extra={<a href="#">More</a>} style={{ width: 300 }} actions={[
-      <BorderTopOutlined key="view" />,
-      <EditOutlined key="edit" />,
-      <DeleteOutlined key="delete" />,
-    ]}>
-    <p>Card content</p>
-    <p>Card content</p>
-    <p>Card content</p>
-  </Card></div>
-  )
-}
+    <div className="m-4">
+      <Card
+        bordered={true}
+        hoverable={true}
+        title="Project Forge"
+        style={{ width: 300 }}
+        className="shadow-md"
+        actions={[
+          <ArrowsAltOutlined key="view" />,
+          <EditOutlined key="edit"/>,
+          <DeleteOutlined key="delete" />,
+        ]}
+      >
+        <p className="text-gray-600 font-normal">Develop a dashboard for a project management application that allows users to manage tasks, and projects.</p>
+      </Card>
+    </div>
+  );
+};
 
-export default ProjectPage
+export default ProjectPage;
